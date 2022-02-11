1a301f53c304d45c0503eb091a5f339c964ab8e5
Moiz-Ur-Rehman <moizurrehman0341@gmail.com>
2022-02-12 00:53:08 +0500

    user login and registration api integerated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
           ? setHeader("header2")
           : setHeader("header");
       }
-    // Similar to componentDidMount and componentDidUpdate:
+    
     useEffect(() => {
       window.addEventListener("scroll", listenScrollEvent)
       return () =>
