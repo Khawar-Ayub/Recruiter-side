83aa57b0c1609a827bea63b739839911152a8c48
Muhammad Khawar Ayub <khaware360@gmail.com>
2022-08-02 06:19:43 +0500

    build uploaded

---
+++ build/precache-manifest.5dd61895fcb4b1c637232a8f31ccba0a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04eee9c208ca23c2e0e9d5942073ddff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4926c031188e0b59db5e",
+    "url": "/static/css/main.6d72bb5c.chunk.css"
+  },
+  {
+    "revision": "4d1bdc204efdd1204ec1",
+    "url": "/static/js/2.6fad25f8.chunk.js"
+  },
+  {
+    "revision": "ef8b747d5019d6976927115dd0c1c44d",
+    "url": "/static/js/2.6fad25f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3982dfe67d89152cf78",
+    "url": "/static/js/3.4d218422.chunk.js"
+  },
+  {
+    "revision": "4926c031188e0b59db5e",
+    "url": "/static/js/main.91248981.chunk.js"
+  },
+  {
+    "revision": "f37755a8c46afef76eea",
+    "url": "/static/js/runtime-main.80f3d3e0.js"
+  },
+  {
+    "revision": "768d8cd120b58f5b536ae06a2534056d",
+    "url": "/static/media/avatar.768d8cd1.jpg"
+  },
+  {
+    "revision": "4089c6bda4a70252a38d018018911a07",
+    "url": "/static/media/bubble.4089c6bd.png"
+  },
+  {
+    "revision": "b74fcb252a72f210cd1ea9fac368b0f8",
+    "url": "/static/media/champion.b74fcb25.png"
+  },
+  {
+    "revision": "c7f4f270301d0924357979de911437fd",
+    "url": "/static/media/hero.c7f4f270.png"
+  },
+  {
+    "revision": "d39e516050f4476ea47453ed9f5d7081",
+    "url": "/static/media/loginImage.d39e5160.svg"
+  },
+  {
+    "revision": "4eef13ad4458446d70e67bc810c716eb",
+    "url": "/static/media/logo.4eef13ad.png"
+  },
+  {
+    "revision": "c65150db0f3889a5aeb8b23115c6f5ce",
+    "url": "/static/media/postjob.c65150db.jpg"
+  },
+  {
+    "revision": "80d7e19de6c7892172ae26fd78fab96e",
+    "url": "/static/media/signupImage.80d7e19d.svg"
+  }
+]);
